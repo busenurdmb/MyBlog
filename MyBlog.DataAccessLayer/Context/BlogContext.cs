75529f04d619ac6b49d85387ba3e3158f00184ed
busenurdmb <bus.dmb@gmail.com>
2024-03-19 20:33:57 +0300

    14.ders izlenip uygalandı.

---
+++ MyBlog.DataAccessLayer/Context/BlogContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyBlog.EntityLayer.Concrete;
+
+
+namespace MyBlog.DataAccessLayer.Context
+{
+    public class BlogContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-493DFJA\\SQLEXPRESS; initial catalog=DbMyBlog;integrated security=true");
+           
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Article> Articles { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Writer> Writers { get; set; }
+
+
+
+    }
+}
